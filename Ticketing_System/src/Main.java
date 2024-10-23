30b7b0dbf4aa6655aa887529ea67ceada57f9538
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-10-23 23:49:44 +0530

    Started to develop Real Time Ticketing System and Created configuration class, getters and setters.

---
+++ Ticketing_System/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Configuration con1 = new Configuration(10,20,30,400);
+        con1.displayConfiguration();
+    }
+}
