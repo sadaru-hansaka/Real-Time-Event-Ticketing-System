70ce86ced757dab55b20adf3864cc5bf7aa0db00
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-11-06 12:08:48 +0530

    Implemented TicketPool with syncronized methods and thread safe ticket queue

--- Ticketing_System/src/main/java/org/example/ticketing_system/TicketPool.java
+++ Ticketing_System/src/main/java/org/example/ticketing_system/TicketPool.java
@@ -1,14 +1,49 @@
 package org.example.ticketing_system;
 
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.stream.Collectors;
+
 public class TicketPool {
 
-//    add ticket function
-    public synchronized void addTickets(){
+//    Used ConcurrentLinkedDeque to provide thread safe
+    private ConcurrentLinkedDeque<Integer> tickets = new ConcurrentLinkedDeque<>();
+    private int ticketID = 0;
 
+//    this should change to maxNumOfTickets
+    private final int maxTicket = 10;
+
+//    add ticket function
+    public synchronized void addTickets(int count){
+        for (int i = 0; i < count && ticketID < maxTicket; i++){
+            tickets.add(ticketID);
+            System.out.println("Ticket added : " + ticketID);
+            ticketID++;
+        }
+        notifyAll();
     }
 
 //    remove ticket function
-    public synchronized void removeTickets(){
+    public synchronized Integer removeTickets(){
+        Integer ticket = tickets.poll();
+        if (ticket != null) {
+            System.out.println("Ticket purchased: " + ticket);
+        } else {
+            System.out.println("No tickets available for purchase.");
+        }
+        return ticket;
+    }
 
+//    Method to check if tickets are available
+    public synchronized boolean hasTickets() {
+        return !tickets.isEmpty();
+    }
+
+    public boolean isMaxReached() {
+        return ticketID >= maxTicket;
+    }
+
+    public List<Integer> getAllTickets() {
+        return tickets.stream().collect(Collectors.toList());
     }
 }
