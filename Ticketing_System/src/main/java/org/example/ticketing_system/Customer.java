a490bad650802ed91ea81582bf885820b50b6e77
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-11-06 12:10:26 +0530

    Added multi-threading functionality for Vendors to add tickets and Customers to purchase tickets concurrently

--- Ticketing_System/src/main/java/org/example/ticketing_system/Customer.java
+++ Ticketing_System/src/main/java/org/example/ticketing_system/Customer.java
@@ -1,17 +1,37 @@
 package org.example.ticketing_system;
 
+import static java.lang.Thread.sleep;
+
 public class Customer implements Runnable{
     private int customerId;
     private int retrievalInterval;
+    private TicketPool ticketPool;
 
-    public Customer(int customerId, int retrievalInterval) {
+    public Customer(int customerId, int retrievalInterval, TicketPool ticketPool) {
         this.customerId = customerId;
         this.retrievalInterval = retrievalInterval;
+        this.ticketPool = ticketPool;
     }
 
 //    run method
     @Override
     public void run() {
-
+        try {
+            while (ticketPool.hasTickets() || !ticketPool.isMaxReached()) {
+                synchronized (ticketPool) {
+                    if (ticketPool.hasTickets()) {
+                        Integer ticket = ticketPool.removeTickets();
+                        if (ticket != null) {
+                            System.out.println("Customer " + customerId + " purchased ticket " + ticket);
+                        }
+                    } else {
+                        System.out.println("Customer " + customerId + " found no tickets available.");
+                    }
+                }
+                sleep(retrievalInterval);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Customer " + customerId + " interrupted.");
+        }
     }
 }
--- Ticketing_System/src/main/java/org/example/ticketing_system/Vendor.java
+++ Ticketing_System/src/main/java/org/example/ticketing_system/Vendor.java
@@ -4,16 +4,26 @@ public class Vendor implements Runnable{
     private int vendorId;
     private int ticketsPerRelease;
     private int releaseInterval;
+    private TicketPool ticketPool;
 
-    public Vendor(int vendorId, int ticketsPerRelease, int releaseInterval) {
+    public Vendor(int vendorId, int ticketsPerRelease, int releaseInterval, TicketPool ticketPool) {
         this.vendorId = vendorId;
         this.ticketsPerRelease = ticketsPerRelease;
         this.releaseInterval = releaseInterval;
+        this.ticketPool= ticketPool;
     }
 
 //    run method
     @Override
     public void run() {
-
+        try{
+            while (!ticketPool.isMaxReached()){
+                ticketPool.addTickets(ticketsPerRelease);
+                System.out.println("Vendor "+vendorId+" released "+ticketsPerRelease+" tickets per release");
+                Thread.sleep(releaseInterval);
+            }
+        }catch (InterruptedException e){
+            System.out.println("Vendor "+vendorId+" interrupted.");
+        }
     }
 }
